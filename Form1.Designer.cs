6dbe6d95037bec6ab4a7f172bd1814badbc681f5
223030469 <223030469@PMB-G20-43>
2026-05-11 17:26:55 +0200

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,198 @@
+﻿namespace Programming
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.dgvBook = new System.Windows.Forms.DataGridView();
+            this.programmingDs = new Programming.programmingDs();
+            this.bookBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.taBook = new Programming.programmingDsTableAdapters.BookTableAdapter();
+            this.bookTitleDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.bookCostDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.qOHDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.bookTitle = new System.Windows.Forms.Label();
+            this.txtBookTitle = new System.Windows.Forms.TextBox();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvBook)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.programmingDs)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bookBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(1487, 816);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.groupBox1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(1479, 790);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Books";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(1479, 790);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "BookOrder";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txtBookTitle);
+            this.groupBox1.Controls.Add(this.bookTitle);
+            this.groupBox1.Controls.Add(this.dgvBook);
+            this.groupBox1.Location = new System.Drawing.Point(41, 42);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(814, 484);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "BookList";
+            // 
+            // dgvBook
+            // 
+            this.dgvBook.AutoGenerateColumns = false;
+            this.dgvBook.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvBook.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.bookTitleDataGridViewTextBoxColumn,
+            this.bookCostDataGridViewTextBoxColumn,
+            this.qOHDataGridViewTextBoxColumn});
+            this.dgvBook.DataSource = this.bookBindingSource;
+            this.dgvBook.Location = new System.Drawing.Point(34, 66);
+            this.dgvBook.Name = "dgvBook";
+            this.dgvBook.Size = new System.Drawing.Size(726, 363);
+            this.dgvBook.TabIndex = 0;
+            // 
+            // programmingDs
+            // 
+            this.programmingDs.DataSetName = "programmingDs";
+            this.programmingDs.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // bookBindingSource
+            // 
+            this.bookBindingSource.DataMember = "Book";
+            this.bookBindingSource.DataSource = this.programmingDs;
+            // 
+            // taBook
+            // 
+            this.taBook.ClearBeforeFill = true;
+            // 
+            // bookTitleDataGridViewTextBoxColumn
+            // 
+            this.bookTitleDataGridViewTextBoxColumn.DataPropertyName = "BookTitle";
+            this.bookTitleDataGridViewTextBoxColumn.HeaderText = "BookTitle";
+            this.bookTitleDataGridViewTextBoxColumn.Name = "bookTitleDataGridViewTextBoxColumn";
+            // 
+            // bookCostDataGridViewTextBoxColumn
+            // 
+            this.bookCostDataGridViewTextBoxColumn.DataPropertyName = "BookCost";
+            this.bookCostDataGridViewTextBoxColumn.HeaderText = "BookCost";
+            this.bookCostDataGridViewTextBoxColumn.Name = "bookCostDataGridViewTextBoxColumn";
+            // 
+            // qOHDataGridViewTextBoxColumn
+            // 
+            this.qOHDataGridViewTextBoxColumn.DataPropertyName = "QOH";
+            this.qOHDataGridViewTextBoxColumn.HeaderText = "QOH";
+            this.qOHDataGridViewTextBoxColumn.Name = "qOHDataGridViewTextBoxColumn";
+            // 
+            // bookTitle
+            // 
+            this.bookTitle.AutoSize = true;
+            this.bookTitle.Location = new System.Drawing.Point(34, 31);
+            this.bookTitle.Name = "bookTitle";
+            this.bookTitle.Size = new System.Drawing.Size(91, 13);
+            this.bookTitle.TabIndex = 1;
+            this.bookTitle.Text = "Enter Book Name";
+            // 
+            // txtBookTitle
+            // 
+            this.txtBookTitle.Location = new System.Drawing.Point(131, 28);
+            this.txtBookTitle.Name = "txtBookTitle";
+            this.txtBookTitle.Size = new System.Drawing.Size(245, 20);
+            this.txtBookTitle.TabIndex = 2;
+            this.txtBookTitle.TextChanged += new System.EventHandler(this.txtBookTitle_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1487, 816);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvBook)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.programmingDs)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bookBindingSource)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.DataGridView dgvBook;
+        private programmingDs programmingDs;
+        private System.Windows.Forms.BindingSource bookBindingSource;
+        private programmingDsTableAdapters.BookTableAdapter taBook;
+        private System.Windows.Forms.DataGridViewTextBoxColumn bookTitleDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn bookCostDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn qOHDataGridViewTextBoxColumn;
+        private System.Windows.Forms.TextBox txtBookTitle;
+        private System.Windows.Forms.Label bookTitle;
+    }
+}
+
